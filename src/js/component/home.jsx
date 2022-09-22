afcc47c68b6ef7b0fa892457977d8cc759ab77af
MikeGajardo <mikegajardo@gmail.com>
2022-09-22 00:57:19 +0000

    8 ball logic

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,22 +5,64 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  let userName = "Michael";
+
+  userName ? console.log(`Hello, ${userName}!`) : console.log("Hello!");
+
+  let userQuestion = "What will happen next week?";
+  console.log(`${userQuestion} ${userName}`);
+
+  let randomNumber = Math.floor(Math.random() * 8);
+
+  let eightBall = "";
+
+  switch (randomNumber) {
+    case 0:
+      eightBall = "It is certain";
+      break;
+    case 1:
+      eightBall = "It is decidedly so";
+      break;
+    case 2:
+      eightBall = "Reply hazy try again";
+      break;
+    case 3:
+      eightBall = "Cannot predict now";
+      break;
+    case 4:
+      eightBall = "Do not count on it";
+      break;
+    case 5:
+      eightBall = "My sources say no";
+      break;
+    case 6:
+      eightBall = "Outlook not so good";
+      break;
+    case 7:
+      eightBall = "Signs point to yes";
+      break;
+    default:
+      eightBall = "please ask again";
+      break;
+  }
+
+  console.log(eightBall);
+
+  return (
+    <div className="text-center">
+      <h1 className="text-center mt-5">Hello Rigo!</h1>
+      <p>
+        <img src={rigoImage} />
+      </p>
+      <a href="#" className="btn btn-success">
+        If you see this green button... bootstrap is working...
+      </a>
+      <p>
+        Made by <a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
+        love!
+      </p>
+    </div>
+  );
 };
 
 export default Home;
